469d00859e6ec0b72ff8ae865e82dc0c4504a513
AJ <abdijabarboxer@gmail.com>
2020-08-02 21:59:40 +0300

    added guide comments to script.js

--- .scripts.js
+++ .scripts.js
@@ -1,6 +1,7 @@
+/*makes function available once page is loaded*/
 $(document).ready(function() {
 
-    
+/* Function to get order from the form*/    
 $("#text-center").submit(function(event) {
 function flavor() {
 var pizzaFlavour = document.getElementById("flavor").value;
@@ -27,7 +28,8 @@ var pizzaNumber = document.getElementById("quantity").value;
 return parseInt(pizzaNumber);
 }
 
-            
+
+/* Constructor create objects from user order*/            
 function Order(flavor, size, crust, topping, quantity) {
 this.newFlavor = flavor;
 this.newSize = size;
@@ -36,12 +38,21 @@ this.newTopping = topping;
 this.newQuantity = quantity;
 }            
 
+/*create object for new order from constructor */
 var userInput = new Order(flavor(), size(), crust(), topping(), number());  
 
+
+/*variable Total cost to collect user total order */
 var totalCost = (userInput.newSize + userInput.newCrust + userInput.newTopping + userInput.newFlavor) *userInput.newQuantity;
 
 
+/*Alert user on the total amout and prompt delivery information */
 alert("Your charges for Pizza" + totalCost);
 prompt("Please enter your phone number");
 prompt("Where are you located");
 alert("Your pizza will be delivered in 1 hour");
+
+/*when clicking submit this prevents submitting the form*/
+event.preventDefault();
+});
+});
