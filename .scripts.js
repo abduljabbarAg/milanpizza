b829e8072887aa068ed5f98a873ffd11356840cd
AJ <abdijabarboxer@gmail.com>
2020-08-02 21:27:35 +0300

    added object to save order from constructor also indented code to script.js

--- .scripts.js
+++ .scripts.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
 
     
-    $("#text-center").submit(function(event) {
+$("#text-center").submit(function(event) {
     
-            function flavor() {
+ function flavor() {
               var pizzaFlavour = document.getElementById("flavor").value;
               return parseInt(pizzaFlavour);
-            }
-            function size() {
-              var pizzaSize = document.getElementById("size").value;
-              return parseInt(pizzaSize);
-            }
-            function crust() {
-              var pizzaCrust = document.getElementById("crust").value;
-              return parseInt(pizzaCrust);
-            }
-            function topping() {
-              var pizzaTopping = document.getElementById("topping").value;
-              return parseInt(pizzaTopping);
-            }
-            function number() {
-              var pizzaNumber = document.getElementById("quantity").value;
-              return parseInt(pizzaNumber);
-            }
+}
+function size() {
+var pizzaSize = document.getElementById("size").value;
+return parseInt(pizzaSize);
+}
+function crust() {
+var pizzaCrust = document.getElementById("crust").value;
+return parseInt(pizzaCrust);
+}
+function topping() {
+var pizzaTopping = document.getElementById("topping").value;
+return parseInt(pizzaTopping);
+}
+function number() {
+var pizzaNumber = document.getElementById("quantity").value;
+return parseInt(pizzaNumber);
+}
 
             
-            function Order(flavor, size, crust, topping, quantity) {
-                this.newFlavor = flavor;
-                this.newSize = size;
-                this.newCrust = crust;
-                this.newTopping = topping;
-                this.newQuantity = quantity;
-              }            
+function Order(flavor, size, crust, topping, quantity) {
+this.newFlavor = flavor;
+this.newSize = size;
+this.newCrust = crust;
+this.newTopping = topping;
+this.newQuantity = quantity;
+}            
+
+var userInput = new Order(flavor(), size(), crust(), topping(), number());  
