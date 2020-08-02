0fb012df107c0f67944437b19ecfb3795cfed846
AJ <abdijabarboxer@gmail.com>
2020-08-02 21:34:10 +0300

    added variable to add size, crust, topping and flavor X Quantity to script.js

--- .scripts.js
+++ .scripts.js
@@ -2,23 +2,26 @@ $(document).ready(function() {
 
     
 $("#text-center").submit(function(event) {
-    
- function flavor() {
-              var pizzaFlavour = document.getElementById("flavor").value;
-              return parseInt(pizzaFlavour);
+function flavor() {
+var pizzaFlavour = document.getElementById("flavor").value;
+return parseInt(pizzaFlavour);
 }
+
 function size() {
 var pizzaSize = document.getElementById("size").value;
 return parseInt(pizzaSize);
 }
+
 function crust() {
 var pizzaCrust = document.getElementById("crust").value;
 return parseInt(pizzaCrust);
 }
+
 function topping() {
 var pizzaTopping = document.getElementById("topping").value;
 return parseInt(pizzaTopping);
 }
+
 function number() {
 var pizzaNumber = document.getElementById("quantity").value;
 return parseInt(pizzaNumber);
@@ -34,3 +37,5 @@ this.newQuantity = quantity;
 }            
 
 var userInput = new Order(flavor(), size(), crust(), topping(), number());  
+
+var totalCost = (userInput.newSize + userInput.newCrust + userInput.newTopping + userInput.newFlavor) *userInput.newQuantity;
